038c7bf1c4e2551f5dff07348c2c6fbcbf7ba007
dcgoss <dcgoss14@gmail.com>
2017-02-05 15:19:30 -0500

    Fixed RCTShadowView bug

--- index.js
+++ index.js
@@ -268,6 +268,7 @@ const styles = StyleSheet.create({
         },
     }),
     flex: 1,
+    flexBasis: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
     alignItems: 'center',
